96f6e374c5dea0c1324ecef3666895eddcf40f2b
Anastasia Abakumova <anastasia.abakumovaa@gmail.com>
2024-05-01 00:22:30 +0300

    final

--- FeedbackWindow.xaml.cs
+++ FeedbackWindow.xaml.cs
@@ -28,6 +28,24 @@ namespace MayaGameLauncher
             CmbCategory.SelectedIndex = 0;
         }
 
+        private void ExitButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
         private void FeedbackButton_Click(object sender, RoutedEventArgs e)
         {
 
